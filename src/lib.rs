289c4b3cc17fa7799d124ffcce594c9891c0388e
Timothy Warrington <tim.warrin@gmail.com>
2020-06-02 17:23:39 -0600

    Init

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+extern crate awful_idea_name;
+extern crate wasm_bindgen;
+
+use awful_idea_name::AwfulIdeaName;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn generate() {
+    AwfulIdeaName::new().generate();
+}
